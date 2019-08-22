1e14b50d3da6d05e23f3e2ab14cec352295751bf
c.friaszapater <c.friaszapater@orwellg.com>
2019-08-22 15:41:11 +0200

    feat: preliminary drag functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DragDropContext } from 'react-beautiful-dnd';
 import initialData from './initial-data';
 import Column from './column';
 
@@ -7,15 +8,26 @@ class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = initialData;
+    this.onDragEnd = (result) => {
+      // TODO
+    };
   }
 
   render () {
-    return this.state.columnOrder.map(columnId => {
-      const column = this.state.columns[columnId];
-      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+    return (
+      <DragDropContext
+        onDragEnd={this.onDragEnd}
+      >
+        {
+          this.state.columnOrder.map(columnId => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
-      return <Column key={column.id} column={column} tasks={tasks} />;
-    });
+            return <Column key={column.id} column={column} tasks={tasks} />;
+          })
+        }
+      </DragDropContext>
+    );
   }
 }
 
