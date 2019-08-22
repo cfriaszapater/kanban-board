c6b4c5277da80606e54b23fd16e68cbd6590fc85
c.friaszapater <c.friaszapater@orwellg.com>
2019-08-22 16:02:42 +0200

    feat: drop functionality

--- src/index.js
+++ src/index.js
@@ -8,10 +8,35 @@ class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = initialData;
-    this.onDragEnd = (result) => {
-      // TODO
-    };
   }
+
+  onDragEnd = result => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn
+      }
+    };
+    this.setState(newState);
+  };
 
   render () {
     return (
