7d26403decbb54f7ecb61f2a3e3649ec1cffcd5d
c.friaszapater <c.friaszapater@orwellg.com>
2019-08-22 17:22:45 +0200

    feat: drag and drop between columns

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 import { DragDropContext } from 'react-beautiful-dnd';
 import initialData from './initial-data';
 import Column from './column';
+
+const Container = styled.div`
+  display: flex;
+`;
 
 class App extends React.Component {
   constructor (props) {
@@ -20,12 +25,21 @@ class App extends React.Component {
       return;
     }
 
-    const column = this.state.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
+    const startCol = this.state.columns[source.droppableId];
+    const endCol = this.state.columns[destination.droppableId];
+    if (startCol === endCol) {
+      this.moveWithinSameColumn(startCol, source, destination, draggableId);
+    } else {
+      this.moveBetweenColumns(startCol, endCol, source, destination, draggableId);
+    }
+  };
+
+  moveWithinSameColumn(startCol, source, destination, draggableId) {
+    const newTaskIds = Array.from(startCol.taskIds);
     newTaskIds.splice(source.index, 1);
     newTaskIds.splice(destination.index, 0, draggableId);
     const newColumn = {
-      ...column,
+      ...startCol,
       taskIds: newTaskIds
     };
     const newState = {
@@ -36,13 +50,36 @@ class App extends React.Component {
       }
     };
     this.setState(newState);
-  };
+  }
+
+  moveBetweenColumns(startCol, endCol, source, destination, draggableId) {
+    const startTaskIds = Array.from(startCol.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStartCol = {
+      ...startCol,
+      taskIds: startTaskIds
+    };
+    const endTaskIds = Array.from(endCol.taskIds);
+    endTaskIds.splice(destination.index, 0, draggableId);
+    const newEndCol = {
+      ...endCol,
+      taskIds: endTaskIds
+    };
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newStartCol.id]: newStartCol,
+        [newEndCol.id]: newEndCol
+      }
+    };
+    this.setState(newState);
+  }
 
   render () {
     return (
-      <DragDropContext
-        onDragEnd={this.onDragEnd}
-      >
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Container>
         {
           this.state.columnOrder.map(columnId => {
             const column = this.state.columns[columnId];
@@ -51,6 +88,7 @@ class App extends React.Component {
             return <Column key={column.id} column={column} tasks={tasks} />;
           })
         }
+        </Container>
       </DragDropContext>
     );
   }
