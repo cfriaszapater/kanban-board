9f50824fc9574a8083f4682d808d3b4a68488e96
c.friaszapater <c.friaszapater@orwellg.com>
2019-08-29 09:56:40 +0200

    remove randomly throw error

--- src/store/cards/createCardAction.ts
+++ src/store/cards/createCardAction.ts
@@ -38,7 +38,6 @@ export const createCard = (card: Task) => async (
     const res = await fetch(req);
     // XXX When "POST /cards", remove ".json"
     const createdCard = (await res.json()).json;
-    randomlyThrowError();
     return dispatch(createCardSuccess(createdCard));
   } catch (ex) {
     return dispatch(createCardFailure(card, ex));
@@ -65,16 +64,4 @@ function createCardFailure(card: Task, error: Error): CreateCardFailureAction {
     type: CREATE_CARD_FAILURE,
     payload: card
   };
-}
-
-function randomlyThrowError() {
-  if (getRandomInt(0, 2)) {
-    throw new Error("simulated network error");
-  }
-}
-
-function getRandomInt(min: number, max: number) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
