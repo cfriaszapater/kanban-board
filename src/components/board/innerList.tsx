3872ddaa93e773e0338a5c39d00efedbec292738
c.friaszapater <c.friaszapater@orwellg.com>
2020-02-07 16:44:25 +0100

    fix: does not render board if card is undefined

--- src/components/board/innerList.tsx
+++ src/components/board/innerList.tsx
@@ -3,14 +3,17 @@ import { CardView } from "./card";
 import { InnerListProps } from "./column";
 export class InnerList extends React.PureComponent<InnerListProps> {
   public render() {
-    return this.props.cards.map((card, index) => (
-      <CardView
-        key={card.id}
-        card={card}
-        index={index}
-        // XXX passing dispatch not needed
-        dispatch={this.props.dispatch}
-      />
-    ));
+    return this.props.cards.map(
+      (card, index) =>
+        card && (
+          <CardView
+            key={card.id}
+            card={card}
+            index={index}
+            // XXX passing dispatch not needed
+            dispatch={this.props.dispatch}
+          />
+        )
+    );
   }
 }
